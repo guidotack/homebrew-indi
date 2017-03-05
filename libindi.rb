635d9b09c21fceb8f9b515bc8525c27148fbe71f
Sean Houghton <sean.houghton@gmail.com>
2017-03-05 08:12:57 -0800

    Working libindi

--- libindi.rb
+++ libindi.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Libindi < Formula
   homepage 'http://www.indilib.org'
-  head 'https://github.com/indilib/indi.git', :using => :git, :tag => '1.3.1'
-  url 'https://github.com/indilib/indi/archive/v1.3.1.zip'
+  head 'https://github.com/indilib/indi.git', :using => :git, :tag => '1.4.1'
+  url 'https://github.com/indilib/indi/archive/v1.4.1.zip'
 
-  version "1.3.1"
+  version "1.4.1"
 
   depends_on 'cmake' => :build
   depends_on 'seanhoughton/astronomy/libnova'
@@ -14,7 +14,7 @@ class Libindi < Formula
   depends_on 'gsl'
 
   def install
-    system "cmake", ".", "-DCMAKE_CXX_FLAGS=-march=core2", *std_cmake_args
+    system "cmake", "libindi", *std_cmake_args
     system "make", "install"
   end
 
