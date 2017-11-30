cb5cf8337a9feb1c697bb88721b667f2b2574fa8
Paweł T. Jochym <jochym@users.noreply.github.com>
2017-11-30 12:29:54 +0100

    Update fftw3 to 3.3.7

--- libfftw3.rb
+++ libfftw3.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libfftw3 < Formula
   homepage 'http://www.fftw.org'
-  url 'http://www.fftw.org/fftw-3.3.3.tar.gz'
-  version "3.3.3"
+  url 'http://www.fftw.org/fftw-3.3.7.tar.gz'
+  version "3.3.7"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--enable-threads"
