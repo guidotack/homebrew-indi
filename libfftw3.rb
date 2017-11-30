523fe6ad93166303d91cad3642731fa545192916
Paweł T. Jochym <jochym@users.noreply.github.com>
2017-11-30 21:00:28 +0100

    Add sha256 to download

--- libfftw3.rb
+++ libfftw3.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Libfftw3 < Formula
   homepage 'http://www.fftw.org'
   url 'http://www.fftw.org/fftw-3.3.7.tar.gz'
+  sha256 '3b609b7feba5230e8f6dd8d245ddbefac324c5a6ae4186947670d9ac2cd25573'
   version "3.3.7"
 
   def install
