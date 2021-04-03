1b48bb73c1d2d76db0324486bd40d091c5d66aed
Guido Tack <guido.tack@monash.edu>
2021-04-03 13:30:33 +1100

    Update to 1.8.9

--- libindi.rb
+++ libindi.rb
@@ -3,18 +3,20 @@ require 'formula'
 class Libindi < Formula
   homepage 'http://www.indilib.org'
   head 'https://github.com/indilib/indi.git', :using => :git
-  url 'https://github.com/indilib/indi/archive/master.zip'
-
-  version "1.4.1-dev"
+  url "https://github.com/indilib/indi/archive/refs/tags/v1.8.9.zip"
+  sha256 "11c3302cdaa1d8e1727f1d00f7b6309e91a80a9ab3c6d7abfb923c09fe091613"
+  version "1.8.9"
 
   depends_on 'cmake' => :build
   depends_on 'libnova'
   depends_on 'libcfitsio'
   depends_on 'libusb'
   depends_on 'gsl'
+  depends_on 'libjpeg'
+  depends_on 'libfftw3'
 
   def install
-    system "cmake", "libindi", *std_cmake_args
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 
