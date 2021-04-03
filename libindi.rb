54acc3a2b77d606c9afb194ffb85bbe708e4fea6
Guido Tack <guido.tack@monash.edu>
2021-04-03 14:08:17 +1100

    Version

--- libindi.rb
+++ libindi.rb
@@ -3,9 +3,8 @@ require 'formula'
 class Libindi < Formula
   homepage 'http://www.indilib.org'
   head 'https://github.com/indilib/indi.git', :using => :git
-  url "https://github.com/indilib/indi/archive/refs/tags/v1.8.9.zip"
-  sha256 "11c3302cdaa1d8e1727f1d00f7b6309e91a80a9ab3c6d7abfb923c09fe091613"
-  version "1.8.9"
+  url "https://github.com/indilib/indi/archive/refs/heads/master.zip"
+  version "1.9.0"
 
   depends_on 'cmake' => :build
   depends_on 'libnova'
